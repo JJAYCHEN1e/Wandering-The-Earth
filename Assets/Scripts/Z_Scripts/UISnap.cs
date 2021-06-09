17634cdc0d1f4e9ef2e0ef49e95a0df6dd714fa7
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2021-06-09 15:18:57 +0800

    完善 UISnap.cs

--- Assets/Scripts/Z_Scripts/UISnap.cs
+++ Assets/Scripts/Z_Scripts/UISnap.cs
@@ -9,6 +9,14 @@ using UnityEngine.UI;
 
 public class UISnap : InteractUI
 {
+    [Header("SnapUp")]
+    public UnityEvent mSnapUp = null;
+    [Header("SnapDown")]
+    public UnityEvent mSnapDown = null;
+    [Header("SnapLeft")]
+    public UnityEvent mSnapLeft = null;
+    [Header("SnapRight")]
+    public UnityEvent mSnapRight = null;
 
     protected override void Start()
     {
@@ -18,25 +26,25 @@ public class UISnap : InteractUI
     public override void OnSnapTurnLeft()
     {
         base.OnSnapTurnLeft();
-
+        if (mSnapLeft != null) mSnapLeft.Invoke();
     }
 
     public override void OnSnapTurnRight()
     {
         base.OnSnapTurnRight();
-
+        if (mSnapRight != null) mSnapRight.Invoke();
     }
 
     public override void OnSnapTurnUp()
     {
         base.OnSnapTurnUp();
-        
+        if (mSnapUp != null) mSnapUp.Invoke();
     }
 
     public override void OnSnapTurnDown()
     {
         base.OnSnapTurnDown();
-        
+        if (mSnapDown != null) mSnapDown.Invoke();
     }
 }
 
